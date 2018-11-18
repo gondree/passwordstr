70f9fcc54d791c47972001a8eb8af784e9a99b2b
Mark Gondree <gondree@gmail.com>
2018-11-18 14:34:55 -0800

    Added placeholders for the util.cpp logic

---
+++ include/util.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <gmpxx.h>
+
+void comb(mpz_t ret, unsigned long int n, unsigned long int r);
+void perm(mpz_t ret, unsigned long int n, unsigned long int r);
+
+
---
+++ src/util.cpp
@@ -0,0 +1,36 @@
+#include <gmpxx.h>
+
+
+// 
+// Computes P(n,r) and stores this into MPZ ret
+//
+// P(n,r) is the total number of r-permutations of a set of 
+//  cardinality n.
+//
+// MPZ ret should already be initialized / allocated
+//   by the caller.
+//
+void perm(mpz_t ret, unsigned long int n, unsigned long int r){
+
+    // TODO
+
+    return;
+}
+
+
+//
+// Computes (n choose r) and stores this into MPZ ret
+//
+// C(n,r) is the total number of r-combinations of a set of 
+//  cardinality n.
+//
+// MPZ ret should already be initialized / allocated
+//   by the caller.
+//
+void comb(mpz_t ret, unsigned long int n, unsigned long int r) {
+
+    // TODO
+
+    return;
+}
+
