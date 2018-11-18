066013acc62837508bed5fa4d17e78f8e2b86cbb
Mark Gondree <gondree@gmail.com>
2018-11-18 14:37:04 -0800

    Added broken skeleton

---
+++ src/broken.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+/* For both perm(n, r) and comb(n,r) below:
+ *  Pick the types for n and r, and the function return type
+ *  to make your logic work for inputs and outputs that are
+ *  as large as possible.
+ */
+
+// TODO: write a new function prototype and implement
+void perm(void) {
+    return;
+}
+
+// TODO: write a new function prototype and implement
+void comb(void) {
+    return;
+}
+
+
+int main(void) {
+
+    // Find values for (n,r) that cause large correct inputs
+    // n = ??
+    // r = n/2
+    cout << "P(n, n/2) = " << endl;
+
+    // show the integer overflow on a larger input
+    cout << "P(n+1, (n+1)/2) = " << endl;
+    return 0;
+}
+
+
